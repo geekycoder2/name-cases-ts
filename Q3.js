0ffb0bcfbe732ee4df1c6a6764b5775578cd3150
Sidra Gondal <150411149+geekycoder2@users.noreply.github.com>
2024-02-16 16:43:07 +0500

    Add files via upload

---
+++ Q3.js
@@ -0,0 +1,13 @@
+"use strict";
+// 3.Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+// LowerCase
+// let personName:string = "SIDRA";
+// let lowerCaseName:string=personName.toLowerCase();
+// console.log(lowerCaseName)
+// UpperCase
+// let personName:string = "sidra";
+// let UpperCaseName:string=personName.toLocaleUpperCase();
+// console.log(UpperCaseName)
+// TittleCase
+// let personName:string = "sidra gondal";
+// console.log(personName.charAt(0).toUpperCase()+personName.slice(1));
